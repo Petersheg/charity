e29b7659686ca6e9be8b7a59598106aae3cd5b50
Peter Oluwasegun <petersheg@gmail.com>
2021-11-20 00:52:51 +0100

    made changes

--- controller/uploads/uploadDP.js
+++ controller/uploads/uploadDP.js
@@ -3,24 +3,26 @@ const User = require('../../model/userModel');
 const catchAsync = require('../../utility/catchAsync');
 const OperationalError = require('../../utility/operationalError');
 
-exports.uploadDP = async (req, res) => {
+exports.uploadDP = catchAsync(
+    async (req, res) => {
 
-    try{
-        const buffer = await sharp(req.file.buffer).resize({width: 500, height: 800}).png().toBuffer();
-
-        const user = req.user;
-        user.profilePicture = buffer;
-        user.save({runValidators : false});
-
-        res.status(200).json({
-            status: "success",
-            message : "upload successful"
-        })
-    }catch(err){
-        console.log(err.message)
-        return next(new OperationalError("something went wrong",400));
+        try{
+            const buffer = await sharp(req.file.buffer).resize({width: 500, height: 800}).png().toBuffer();
+    
+            const user = req.user;
+            user.profilePicture = buffer;
+            user.save({runValidators : false});
+    
+            res.status(200).json({
+                status: "success",
+                message : "upload successful"
+            })
+        }catch(err){
+            console.log(err.message)
+            return next(new OperationalError("something went wrong",400));
+        }
     }
-}
+)
 
 exports.getDP = catchAsync(
     async(req,res)=>{
