0c818aaedb500ae2f14b89127614f76d02eca8cb
Peter Oluwasegun <petersheg@gmail.com>
2022-01-09 22:01:36 +0100

    made changes to admin route

--- route/admin.js
+++ route/admin.js
@@ -5,10 +5,10 @@ const userCon = require('../controller/admin/manageUser/user');
 const productCon = require('../controller/admin/manageProduct/products');
 
 const router = express.Router();
-router.post('/:Id/delete_user',secureRoute,authorize('admin'),userCon.deleteUser);
-router.post('/:Id/delete_product',secureRoute,authorize('admin'),productCon.deleteProduct);
-router.post('/:Id/delete_deal',secureRoute,authorize('admin'),productCon.deleteDOD);
-router.post('/:Id/decide_on_deals',secureRoute,authorize('admin'),productCon.respondToDealOfTheDay);
+router.post('/:Id/delete_user',secureRoute,authorize('super admin','admin'),userCon.deleteUser);
+router.post('/:Id/delete_product',secureRoute,authorize('super admin','admin'),productCon.deleteProduct);
+router.post('/:Id/delete_deal',secureRoute,authorize('super admin','admin'),productCon.deleteDOD);
+router.post('/:Id/decide_on_deals',secureRoute,authorize('super admin','admin'),productCon.respondToDealOfTheDay);
 router.post('/add_admin',secureRoute,authorize('super admin'),userCon.registerAdmin);
 router.post('/:Id/edit_admin_details',secureRoute,authorize('super admin','admin'),userCon.editAdminDetails);
 router.post('/:userId/toggle_admin_account',secureRoute,authorize('super admin','admin'),userCon.toggleAccount);
